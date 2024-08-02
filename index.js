c0626baf495b8b194c06518ec43c888e5cf3279a
Atanu Mojumdar <atanu192003mojumdar@gmail.com>
2024-08-02 07:21:55 +0530

    error handling

--- index.js
+++ index.js
@@ -11,28 +11,40 @@ let PORT = 8000
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/health", async (req, res) => {
-    let propmt = req.body?.prompt;
-    if(!propmt){
-        propmt = "Hello buddy, greet me with nice words"
-    }
-    const answer = await askAI(propmt);
+    try {
+        let propmt = req.body?.prompt;
+        if (!propmt) {
+            propmt = "Hello buddy, greet me with nice words"
+        }
+        const answer = await askAI(propmt);
         return res.status(200).json({
-            message: answer.toString(),
+            data: answer.toString(),
             success: true,
+            message: "server is healthy"
         })
+    }
+    catch (err) {
+        console.log("AI error: ")
+        console.log(err)
+        return res.status(400).json({
+            success: false,
+            message: "server is unhealthy"
+        })
+    }
+
 })
 
 if (cluster.isMaster) {
     console.log("master: ");
     for (let i = 0; i < numCPUs; i++) {
-     cluster.fork();
+        cluster.fork();
     }
 }
-else{
+else {
     app.listen(PORT, () => {
         console.log(`Listening on http://localhost:${PORT}`)
     })
-}   
+}
 
 
 
